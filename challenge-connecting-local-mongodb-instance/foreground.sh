a1a0de89370663aae46aa2aac2b567a6d04a5d66
jenunes <jean_nsilva@hotmail.com>
2021-10-28 10:24:13 -0300

    foreground

--- challenge-connecting-local-mongodb-instance/foreground.sh
+++ challenge-connecting-local-mongodb-instance/foreground.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-echo "+-----------------------------------------+"
-echo "| Welcome to Getting Started with MongoDB |"
-echo "+-----------------------------------------+"	
++-----------------------------------------+
+| Welcome to Getting Started with MongoDB |
++-----------------------------------------+	
 printf 'This is a bash shell script.\n\n'
 printf 'Any commands you put in here will be executed at runtime.\n\n'
 printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
