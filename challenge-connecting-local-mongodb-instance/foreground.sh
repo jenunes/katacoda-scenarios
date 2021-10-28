466798b9d0547b3cc8073b863cf7f37961dda6ee
jenunes <jean_nsilva@hotmail.com>
2021-10-28 10:19:50 -0300

    foreground

--- challenge-connecting-local-mongodb-instance/foreground.sh
+++ challenge-connecting-local-mongodb-instance/foreground.sh
@@ -1,13 +1,5 @@
 #/bin/bash
 
-
-echo'   ______     __  __  _                _____ __             __           __            _ __  __       __  ___                        ____  ____ '
-echo'  / ____/__  / /_/ /_(_)___  ____ _   / ___// /_____ ______/ /____  ____/ /  _      __(_) /_/ /_     /  |/  /___  ____  ____ _____  / __ \/ __ )'
-echo' / / __/ _ \/ __/ __/ / __ \/ __ `/   \__ \/ __/ __ `/ ___/ __/ _ \/ __  /  | | /| / / / __/ __ \   / /|_/ / __ \/ __ \/ __ `/ __ \/ / / / __  |'
-echo'/ /_/ /  __/ /_/ /_/ / / / / /_/ /   ___/ / /_/ /_/ / /  / /_/  __/ /_/ /   | |/ |/ / / /_/ / / /  / /  / / /_/ / / / / /_/ / /_/ / /_/ / /_/ / '
-echo'\____/\___/\__/\__/_/_/ /_/\__, /   /____/\__/\__,_/_/   \__/\___/\__,_/    |__/|__/_/\__/_/ /_/  /_/  /_/\____/_/ /_/\__, /\____/_____/_____/  '
-echo'                          /____/                                                                                     /____/                     '
-
 printf 'This is a bash shell script.\n\n'
 printf 'Any commands you put in here will be executed at runtime.\n\n'
 printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
