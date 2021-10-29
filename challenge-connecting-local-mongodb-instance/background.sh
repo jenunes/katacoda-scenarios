a74f89c1d44534cae5e47f55e1e5dcc3a1fe2971
jenunes <jean_nsilva@hotmail.com>
2021-10-29 01:15:06 -0300

    Steps

--- challenge-connecting-local-mongodb-instance/background.sh
+++ challenge-connecting-local-mongodb-instance/background.sh
@@ -3,7 +3,7 @@
 cd /tmp/
 wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/4.4/multiverse/binary-amd64/mongodb-org-server_4.4.10_amd64.deb
 wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/4.4/multiverse/binary-amd64/mongodb-org-shell_4.4.10_amd64.deb
-https://github.com/neelabalan/mongodb-sample-dataset/raw/main/sample_airbnb/listingsAndReviews.json
+wget https://github.com/neelabalan/mongodb-sample-dataset/raw/main/sample_airbnb/listingsAndReviews.json
 sudo dpkg -i mongodb-org-*
 systemctl start mongod
 apt install mongo-tools -y
