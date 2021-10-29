d260ac916b168063d1f7bf75fecfcad0948dc453
jenunes <jean_nsilva@hotmail.com>
2021-10-29 00:44:07 -0300

    Steps

--- challenge-connecting-local-mongodb-instance/background.sh
+++ challenge-connecting-local-mongodb-instance/background.sh
@@ -5,4 +5,6 @@ wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/4.4/multiverse/
 wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/4.4/multiverse/binary-amd64/mongodb-org-shell_4.4.10_amd64.deb
 sudo dpkg -i mongodb-org-*
 systemctl start mongod
+sleep 5;
 
+mongo localhost:27017/module1 --eval "db.createUser({user:'read_user', pwd:'gswmongo', roles: ['read']})";
