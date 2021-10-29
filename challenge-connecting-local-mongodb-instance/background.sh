b0b14a51a7c9351adec8ee75821a827e7fd59c8f
jenunes <jean_nsilva@hotmail.com>
2021-10-29 01:07:58 -0300

    Steps

--- challenge-connecting-local-mongodb-instance/background.sh
+++ challenge-connecting-local-mongodb-instance/background.sh
@@ -3,8 +3,10 @@
 cd /tmp/
 wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/4.4/multiverse/binary-amd64/mongodb-org-server_4.4.10_amd64.deb
 wget https://repo.mongodb.org/apt/ubuntu/dists/focal/mongodb-org/4.4/multiverse/binary-amd64/mongodb-org-shell_4.4.10_amd64.deb
+https://github.com/neelabalan/mongodb-sample-dataset/raw/main/sample_airbnb/listingsAndReviews.json
 sudo dpkg -i mongodb-org-*
 systemctl start mongod
-sleep 5;
-
+apt install mongo-tools -y
+sleep 3;
 mongo localhost:27017/module1 --eval "db.createUser({user:'read_user', pwd:'gswmongo', roles: ['read']})";
+mongoimport --drop --host localhost --port 27017 --db module1 --collection reviews --file listingsAndReviews.json
